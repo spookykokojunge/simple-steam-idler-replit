29189a1f241ca8b5a7f4483ce21569b4dae69c3c
Spooky <157039814+spookykokojunge@users.noreply.github.com>
2024-05-23 19:38:49 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,36 +1,43 @@
 const steamUser = require('steam-user');
 const steamTotp = require('steam-totp');
-const keep_alive = require('./keep_alive.js')
+const keep_alive = require('./keep_alive.js');
 
 var username = process.env.username;
 var password = process.env.password;
 var shared_secret = process.env.shared;
 
-var games = [730, 440, 570];  // Enter here AppIDs of the needed games
+var games = [730, 440, 570];  // AppIDs of the needed games
 var status = 1;  // 1 - online, 7 - invisible
+var customGame = "My Custom Game";  // The custom game name
 
+var switchInterval = 30000;  // Time in milliseconds between switching games (e.g., 30000ms = 30 seconds)
 
-user = new steamUser();
-user.logOn({"accountName": username, "password": password, "twoFactorCode": steamTotp.generateAuthCode(shared_secret)});
-user.on('loggedOn', () => {
-	if (user.steamID != null) console.log(user.steamID + ' - Successfully logged on');
-	user.setPersona(status);               
-	user.gamesPlayed(games);
+const user = new steamUser();
+user.logOn({
+    "accountName": username,
+    "password": password,
+    "twoFactorCode": steamTotp.generateAuthCode(shared_secret)
 });
 
+user.on('loggedOn', () => {
+    if (user.steamID != null) console.log(user.steamID + ' - Successfully logged on');
+    user.setPersona(status);               
+    switchGames();
+});
 
-// var username2 = process.env.username2;
-// var password2 = process.env.password2;
-// var shared_secret2 = process.env.shared2;
+function switchGames() {
+    // Start with the custom game
+    user.gamesPlayed([{ "game_id": "0", "game_extra_info": customGame }]);
+    console.log('Playing custom game: ' + customGame);
 
-// var games2 = [730, 440, 570, 304930];  // Enter here AppIDs of the needed games
-// var status2 = 1;  // 1 - online, 7 - invisible
+    setTimeout(() => {
+        // Switch to actual games
+        user.gamesPlayed(games);
+        console.log('Playing actual games: ' + games.join(', '));
 
-
-// user2 = new steamUser();
-// user2.logOn({"accountName": username2, "password": password2, "twoFactorCode": steamTotp.generateAuthCode(shared_secret2)});
-// user2.on('loggedOn', () => {
-// 	if (user2.steamID != null) console.log(user2.steamID + ' - Successfully logged on');
-// 	user2.setPersona(status2);               
-// 	user2.gamesPlayed(games2);
-// });
+        setTimeout(() => {
+            // Call the function recursively to switch back to the custom game
+            switchGames();
+        }, switchInterval);
+    }, switchInterval);
+}
